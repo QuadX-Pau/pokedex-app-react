566b13cea777a8ad3b3c4763c343ab6565e04977
QuadX-Elaine <emhernandez@quadx.xyz>
2023-09-15 19:15:49 +0800

    Add Pokedex app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import './index.css';
 import './App.css';
 
-function App() {
+import React, { useEffect, useState } from 'react';
+
+import PokeCard from './components/PokeCard';
+
+const App = () => {
+
+   const[allPokemons, setAllPokemons] = useState([])
+   const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+      
+  const getAllPokemons = async () => {
+    const res = await fetch(loadMore)
+    const data = await res.json()
+
+    setLoadMore(data.next)
+
+    function createPokemonObject(results)  {
+      results.forEach( async pokemon => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        const data =  await res.json()
+        setAllPokemons( currentList => [...currentList, data])
+        await allPokemons.sort((a, b) => a.id - b.id)
+      })
+    }
+    createPokemonObject(data.results)
+  }
+
+ useEffect(() => {
+  getAllPokemons()
+ }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="topbar-container">
+        <h1>Pokédex</h1>
+      </div>
+      <div className="pokemon-container">
+        <div className="all-container">
+          
+          {allPokemons.map( (pokemonStats, index) => 
+          <button className= "card-button">
+            <PokeCard
+              key={index}
+              id={pokemonStats.id}
+              image={pokemonStats.sprites.other.dream_world.front_default}
+              name={pokemonStats.name}
+              type={pokemonStats.types[0].type.name}
+            />
+            </button>
+            )}
+          
+        </div>
+
+          <button className="load-more" onClick={() => getAllPokemons()}>Load more</button>
+      </div>
     </div>
   );
 }
